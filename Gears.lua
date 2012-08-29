b405324ab16e6f3ddc8fb0677e01b97695aed116
StormFX <stormfx@gmail.com>
2012-08-29 14:06:33 +0000

    - ToC to 50001. - Update version.

--- Gears.lua
+++ Gears.lua
@@ -15,7 +15,7 @@ if not MSQ then return end
 -- Gears
 MSQ:AddSkin("Gears", {
 	Author = "StormFX, Unknown",
-	Version = "4.3.@project-revision@",
+	Version = "5.0.@project-revision@",
 	Shape = "Circle",
 	Masque_Version = 40300,
 	Backdrop = {
